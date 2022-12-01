d7fb13f51359392894c0a3518fbbc2b1bf5fa3ba
Ritesh Kumar <ritesh@emerald.pink>
2022-12-01 17:22:12 -0500

    Day 1: Basic python solution
    This solution is intended to be more of a teaching tool for someone that
    wants to pick up python.

--- 2022/day1/day1_ritesh.py
+++ 2022/day1/day1_ritesh.py
@@ -1,3 +1,131 @@
 #!/usr/bin/env python3
+"""
+--- Day 1: Calorie Counting ---
 
-print("Hello Github")
+Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
+
+To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+
+The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line. Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
+
+For example, suppose the Elves finish writing their items' Calories and end up with the following list:
+
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+This list represents the Calories of the food carried by five Elves:
+
+    The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+    The second Elf is carrying one food item with 4000 Calories.
+    The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
+    The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
+    The fifth Elf is carrying one food item with 10000 Calories.
+
+In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
+
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+
+--- Part Two ---
+
+By the time you calculate the answer to the Elves' question, they've already realized that the Elf carrying the most Calories of food might eventually run out of snacks.
+
+To avoid this unacceptable situation, the Elves would instead like to know the total Calories carried by the top three Elves carrying the most Calories. That way, even if one of those Elves runs out of snacks, they still have two backups.
+
+In the example above, the top three Elves are the fourth Elf (with 24000 Calories), then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories). The sum of the Calories carried by these three elves is 45000.
+
+Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+
+"""
+
+def parse_input(url, readlines=False):
+    """
+    Parses input file via provided url.
+    Returns lines read as a list if readlines is True.
+    """
+
+    try:
+        with open(url, 'r') as file:
+            content = file.readlines() if readlines else file.read()
+
+            return content
+
+    except Exception as e:
+        msg = f"Error reading input file: {e}"
+        print(msg)
+
+    return None
+
+def solve_part_1(input_lines):
+    max_elf_sum = 0
+    current_elf_sum = 0
+
+    for line in input_lines:
+        if not line.strip(): # empty line
+            max_elf_sum = max(max_elf_sum, current_elf_sum)
+            current_elf_sum = 0
+
+            continue
+
+        current_elf_sum += int(line)
+
+    return max_elf_sum
+
+def solve_part_2(input_lines):
+    inventories = []
+    current_sum = 0
+
+    for line in input_lines:
+
+        if not line.strip(): # empty line
+            inventories.append(current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(line)
+
+    if current_sum > 0: # if there's no space line after last line
+        inventories.append(current_sum)
+
+    inventories.sort(reverse=True)
+
+    top_3 = inventories[0] + inventories[1] + inventories[2]
+
+    return top_3
+
+
+def main():
+    input_lines = parse_input('./input.in', readlines=True)
+
+    if not input_lines:
+        msg = "Why even bother!?"
+        print(msg)
+
+        return
+
+    part_1_solution = solve_part_1(input_lines)
+    part_2_solution = solve_part_2(input_lines)
+
+    print(f"part 1 solution: {part_1_solution} ")
+    print(f"part 2 solution: {part_2_solution} ")
+
+    return
+
+
+if __name__ == "__main__":
+    main()
